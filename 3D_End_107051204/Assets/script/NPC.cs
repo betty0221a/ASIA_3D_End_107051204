3d179c5ba3354d53ad31b29f5d896f14b8735794
betty0221a <betty0221a@gmail.com>
2021-01-16 15:37:12 +0800

    對話程式

---
+++ 3D_End_107051204/Assets/script/NPC.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCdeta deta;
+
+
+    public bool playerInArea;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "狗狗")
+        {
+            playerInArea = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name =="狗狗")
+        {
+            playerInArea = false;
+        }
+    }
+
+    private void dialogue()
+    {
+        print(deta.dialogueA);
+    }
+
+}
