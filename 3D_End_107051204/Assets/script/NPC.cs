6a5d5f15b3a723b77c93742736455113d86d4387
betty0221a <betty0221a@gmail.com>
2021-01-17 11:34:57 +0800

    對話框顯示

--- 3D_End_107051204/Assets/script/NPC.cs
+++ 3D_End_107051204/Assets/script/NPC.cs
@@ -1,10 +1,21 @@
 ﻿
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class NPC : MonoBehaviour
 {
     [Header("NPC資料")]
     public NPCdeta deta;
+    [Header("對話框")]
+    public GameObject dialogue;
+    [Header("對話內容")]
+    public Text textContent;
+    [Header("對話者名稱")]
+    public Text textName;
+    [Header("對話間隔")]
+    public float interval = 0.2f;
+
 
 
     public bool playerInArea;
@@ -13,6 +24,10 @@ public class NPC : MonoBehaviour
         if (other.name == "狗狗")
         {
             playerInArea = true;
+           StartCoroutine (Dialogue());
+            
+           
+        
         }
     }
 
@@ -21,12 +36,36 @@ public class NPC : MonoBehaviour
         if (other.name =="狗狗")
         {
             playerInArea = false;
+            StopDialogue();
+           
+            
+        }
+        
+    }
+    private void StopDialogue()
+    {
+        dialogue.SetActive(false);
+        StopAllCoroutines();
+    }
+    private IEnumerator Dialogue()
+    {
+
+        dialogue.SetActive(true);
+        textContent.text = "";
+        for (int i = 0; i < deta.dialogueA.Length; i++)
+        {
+            // print(deta.dialogueA[i]);
+            textContent.text += deta.dialogueA[i] + "";        
+            yield return new WaitForSeconds(interval);
         }
     }
+    
+        
+    
 
-    private void dialogue()
-    {
-        print(deta.dialogueA);
-    }
+    
 
+   
+
+			
 }
