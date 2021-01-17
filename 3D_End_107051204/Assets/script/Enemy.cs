f069560ccb960f8dbff20240ee1ee0230402e81a
betty0221a <betty0221a@gmail.com>
2021-01-17 12:44:17 +0800

    冷卻時間設定完成

---
+++ 3D_End_107051204/Assets/script/Enemy.cs
@@ -0,0 +1,68 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+   [Header("移動速度"), Range(0, 50)]
+    public float speed = 3;
+    [Header("停止距離"), Range(0, 50)]
+    public float stopDistance = 2.5f;
+    [Header("攻擊冷卻時間"), Range(0, 50)]
+    public float cd = 2.5f;
+
+    private Transform player;
+    private NavMeshAgent nav;
+    private Animator ani;
+    /// <summary>
+    /// 計時器
+    /// </summary>
+    private float timer;
+
+
+    private void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
+        player = GameObject.Find("狗狗").transform;
+
+        nav.speed = speed;
+        nav.stoppingDistance = stopDistance;
+    }
+
+    private void Update()
+    {
+        Track();
+        Attack();
+    }
+
+    private void Attack()
+    {
+        if(nav.remainingDistance < stopDistance)
+        {
+            timer += Time.deltaTime;
+
+            Vector3 pos = player.position;
+            pos.y = transform.position.y;
+
+            transform.LookAt(player);
+            if(timer >= cd)
+                {
+                ani.SetTrigger("攻擊觸發");
+                timer = 0;
+            }
+            
+        }
+    }
+    /// <summary>
+    /// 追蹤
+    /// </summary>
+    private void Track()
+    {
+        nav.SetDestination(player.position);
+        ani.SetBool("走路開關", nav.remainingDistance > stopDistance);
+
+       // print("剩餘的距離:" + nav.remainingDistance);
+
+    }
+}
